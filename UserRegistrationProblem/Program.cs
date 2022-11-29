75f09dc9a37918bcef32c9b6bc76a85630f332b8
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-29 12:56:42 +0530

    [Lalit] Add: Adding User First Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,7 +5,25 @@ namespace UserRegistrationProblem
     {
         public static void Main(string[] args)
         {
+            UserRegistration user = new UserRegistration();
             Console.WriteLine("Welcome to User Registration Problem Statements.");
+            bool choice = true;
+            while (choice)
+            {
+                Console.WriteLine("Enter your choice");
+                Console.WriteLine("1. Enter First Name\n2. Exit");
+                int condition = Convert.ToInt32(Console.ReadLine());
+                switch (condition)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name - ");
+                        user.FirstName();
+                        break;
+                    default:
+                        Console.WriteLine("Try Again! Bro.");
+                        break;
+                }
+            }
         }
     }
 }
