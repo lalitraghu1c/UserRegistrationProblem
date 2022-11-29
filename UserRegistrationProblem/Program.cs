883739044c4b7afc2eef7b5a7bb67af5c877917d
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-29 12:43:34 +0530

    [Lalit] Add: Folder Structure Created

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem Statements.");
+        }
+    }
+}
